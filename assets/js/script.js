ffc672641af572aaf637881a711dd41b430553c6
AzizTeqnite123 <azizteqnite@gmail.com>
2025-06-13 23:52:38 +0500

    second-commit

---
+++ assets/js/script.js
@@ -0,0 +1,48 @@
+// SCRIPT FOR ACCORDIANS FAQS
+
+document.addEventListener('DOMContentLoaded', function () {
+    function faqsAccordion() {
+        var accordionSections = document.querySelectorAll(".accordion-item");
+
+        // Ensure the first item is active by default
+        if (accordionSections.length > 0) {
+            var firstAccordionItemHeader = accordionSections[0].querySelector(".accordion-item-header");
+            var firstAccordionItemBody = firstAccordionItemHeader.nextElementSibling;
+
+            if (firstAccordionItemBody) {
+                firstAccordionItemHeader.classList.add("active");
+                firstAccordionItemBody.style.maxHeight = firstAccordionItemBody.scrollHeight + "px";
+            }
+        }
+
+        accordionSections.forEach(section => {
+            var accordionItemHeaders = section.querySelector(".accordion-item-header");
+            var accordionItemBody = section.querySelector(".accordion-item-body");
+
+            accordionItemHeaders.addEventListener("click", function () {
+                var currentlyActiveHeader = document.querySelector(".accordion-item-header.active");
+                var currentlyActiveBody = currentlyActiveHeader ? currentlyActiveHeader.nextElementSibling : null;
+
+                // Close the previously active item
+                if (currentlyActiveHeader && currentlyActiveBody) {
+                    currentlyActiveHeader.classList.remove("active");
+                    currentlyActiveBody.style.maxHeight = 0;
+                }
+
+                // Open the clicked item
+                if (accordionItemHeaders !== currentlyActiveHeader) {
+                    accordionItemHeaders.classList.add("active");
+                    accordionItemBody.style.maxHeight = accordionItemBody.scrollHeight + "px";
+                } else {
+                    // If the clicked item was already active, close it
+                    accordionItemHeaders.classList.remove("active");
+                    accordionItemBody.style.maxHeight = 0;
+                }
+            });
+        });
+    }
+
+    faqsAccordion();
+});
+
+// SCRIPT FOR ACCORDIANS FAQS
